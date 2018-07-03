c35b6002a391a24b01c685174636297b0ed1c19f
LingSuo <LingSuoForCHY@163.com>
2018-07-03 14:44:17 +0800

    Initial Commit

---
+++ LabelColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LabelColor
+//
+//  Created by Arno on 2018/7/3.
+//  Copyright © 2018年 Arno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
