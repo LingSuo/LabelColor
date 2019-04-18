bb7ccca867e70c51e5ec09ac9ef4a56df98bc284
LingSuo <LingSuoForCHY@163.com>
2019-04-18 15:06:16 +0800

    chy

--- LabelColor/AppDelegate.m
+++ LabelColor/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"23");
+    NSLog(@"234");
     return YES;
 }
 
